66eefc0cc29ed67100538342bf10bb8ef1a2b6c3
kitkat119 <gprydal@yahoo.co.uk>
2017-08-15 22:14:34 +0100

    adds weather api allowing user discover current temp in city of their choosing

--- js/interface.js
+++ js/interface.js
@@ -23,6 +23,12 @@ $('#power-save').click(function() {
   updateTemp();
 })
 
+$('#select-city').submit(function(event) {
+  event.preventDefault();
+  var city = $('#current-city').val();
+  updateWeather(city);
+});
+
 function updateTemp() {
   $('#temp').text(thermostat.temperature());
   $('#temp').attr('class', thermostat.energyUsage());
@@ -31,4 +37,13 @@ function updateTemp() {
 function updatePowerSaving() {
   $('#power-saving').text("Power saving mode is " + thermostat.isPowerSaving());
 }
+function updateWeather(city) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+  var token = '&appid=1165259d6e26ca0f8e03387e1f2da5ac';
+  var units = '&units=metric';
+    $.get(url + token + units, function(response) {
+      $('#current-temp').text(Math.round(response.main.temp));
+      $('#city').text(response.name);
+     });
+  }
 });
